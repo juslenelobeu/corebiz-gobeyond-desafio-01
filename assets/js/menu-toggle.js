63b9f2cb99c1c6e2ef575e5a590211dc9ed7d764
juslenelobeu <juslenelobeu@gmail.com>
2021-09-13 17:04:07 -0300

    :sparkles: dropdown implemented

--- assets/js/menu-toggle.js
+++ assets/js/menu-toggle.js
@@ -1,6 +1,8 @@
 const toggle = document.querySelector(".header__button-toggle");
 const toggleClose = document.querySelector(".header__button-toggle--close");
 const nav = document.querySelector(".header__nav");
+const linkDropdown = document.querySelector(".nav__dropdown--content");
+const dropdownContent = document.querySelector(".nav__link--dropdown");
 
 toggle.addEventListener("click", () => {
   nav.classList.add("visible");
@@ -10,4 +12,8 @@ toggle.addEventListener("click", () => {
 toggleClose.addEventListener("click", () => {
   nav.classList.remove("visible");
   toggleClose.classList.remove("visible");
+});
+
+linkDropdown.addEventListener("click", () => {
+  dropdownContent.classList.add("visible");
 });
